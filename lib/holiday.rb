354afa484f75d34126c75d83b2e6094694f70ba0
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 22:14:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,7 +31,8 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-   
+   holiday.each do |holiday_name, supplies|
+     puts "  "
 
 end
 
