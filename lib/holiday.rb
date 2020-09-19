d96e9d5d715942ec5d1ddab35eef78581df6a6a2
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 22:29:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |seasons, holidays|
     holidays.map do |holiday_name, supplies|
-      
+      supplies.include?("BBQ") ? holiday_name : nil
     
     
 end
