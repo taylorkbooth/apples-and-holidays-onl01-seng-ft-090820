dcbf5ef61bff41d3e86015e488d86e6504b265e2
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 21:41:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -7,6 +7,7 @@ end
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash[:winter][:christmas] << supply
   # holiday_hash[:winter][:new_years] << supply
+  holiday_hash
 
 end
 
